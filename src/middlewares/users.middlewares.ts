089ba534c5a19dcdb732f2860ce5686b1ec77bc8
DucNghia2004 <caoducnghia1605@gmail.com>
2025-09-16 14:43:48 +0700

    Tạo User Schema

--- src/middlewares/users.middlewares.ts
+++ src/middlewares/users.middlewares.ts
@@ -1,4 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
+import User from '~/models/schemas/User.schema'
+import databaseService from '~/services/database.services'
+import usersService from '~/services/users.services'
 
 export const loginValidator = (req: Request, res: Response, next: NextFunction) => {
   const { email, password } = req.body
@@ -8,4 +11,21 @@ export const loginValidator = (req: Request, res: Response, next: NextFunction) 
     })
   }
   next()
+}
+
+export const registerController = async (req: Request, res: Response) => {
+  const { email, password } = req.body
+  try {
+    const result = await usersService.register({ email, password })
+    // await databaseService.users.find({})
+    return res.json({
+      message: 'Register success',
+      result
+    })
+  } catch (error) {
+    return res.status(400).json({
+      message: 'Register failed',
+      error
+    })
+  }
 }
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import { loginController } from '~/controllers/users.controllers'
-import { loginValidator } from '~/middlewares/users.middlewares'
+import { loginValidator, registerController } from '~/middlewares/users.middlewares'
 const usersRouter = express.Router()
 
 // usersRouter.use((req, res, next) => {
@@ -9,5 +9,6 @@ const usersRouter = express.Router()
 // })
 
 usersRouter.post('/login', loginValidator, loginController)
+usersRouter.post('/register', registerController)
 
 export default usersRouter
