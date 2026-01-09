bc6b2b9414f5a52da689bd550510b1286a513337
DucNghia2004 <caoducnghia1605@gmail.com>
2026-01-09 11:08:05 +0700

    Tạo Bookmark

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import { UPLOAD_IMAGE_DIR, UPLOAD_VIDEO_DIR } from './constants/dir'
 import staticRouter from './routes/static.routes'
 import cors from 'cors'
 import tweetsRouter from './routes/tweets.routes'
+import bookmarksRouter from './routes/bookmarks.routes'
 
 config()
 
@@ -35,6 +36,7 @@ app.get('/', (req, res) => {
 app.use('/users', usersRouter)
 app.use('/medias', mediasRouter)
 app.use('/tweets', tweetsRouter)
+app.use('/bookmarks', bookmarksRouter)
 
 app.use('/static', staticRouter)
 app.use('/static/video', express.static(UPLOAD_VIDEO_DIR))
---
+++ src/services/bookmarks.services.ts
@@ -0,0 +1,49 @@
+import { ObjectId, WithId } from 'mongodb'
+import Bookmark from '~/models/schemas/Bookmark.schema'
+
+import databaseService from '~/services/database.services'
+
+class BookmarkService {
+  // async bookmarkTweet(user_id: string, tweetId: string) {
+  //   const result = await databaseService.bookmarks.findOneAndUpdate(
+  //     {
+  //       user_id: new ObjectId(user_id),
+  //       tweet_id: new ObjectId(tweetId)
+  //     },
+  //     {
+  //       $setOnInsert: new Bookmark({
+  //         user_id: new ObjectId(user_id),
+  //         tweet_id: new ObjectId(tweetId)
+  //       })
+  //     },
+  //     {
+  //       upsert: true,
+  //       returnDocument: 'after'
+  //     }
+  //   )
+  //   return result.value as WithId<Bookmark>
+  // }
+  async bookmarkTweet(user_id: string, tweetId: string) {
+    await databaseService.bookmarks.updateOne(
+      {
+        user_id: new ObjectId(user_id),
+        tweet_id: new ObjectId(tweetId)
+      },
+      {
+        $setOnInsert: new Bookmark({
+          user_id: new ObjectId(user_id),
+          tweet_id: new ObjectId(tweetId)
+        })
+      },
+      { upsert: true }
+    )
+
+    return (await databaseService.bookmarks.findOne({
+      user_id: new ObjectId(user_id),
+      tweet_id: new ObjectId(tweetId)
+    })) as WithId<Bookmark>
+  }
+}
+
+const bookmarkService = new BookmarkService()
+export default bookmarkService
