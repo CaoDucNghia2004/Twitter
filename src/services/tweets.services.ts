dd228b4c995f13fce7d79ee30bcae642a21beed8
DucNghia2004 <caoducnghia1605@gmail.com>
2026-01-09 10:43:42 +0700

    Tạo hashtags

--- src/services/tweets.services.ts
+++ src/services/tweets.services.ts
@@ -1,15 +1,57 @@
-import { ObjectId } from 'mongodb'
+import { ObjectId, WithId } from 'mongodb'
 import { TweetRequestBody } from '~/models/requests/Tweet.requests'
+import Hashtag from '~/models/schemas/Hashtag.schema'
 import Tweet from '~/models/schemas/Tweet.schema'
 import databaseService from '~/services/database.services'
 
 class TweetsService {
+  // async checkAndCreateHashtags(hashtags: string[]) {
+  //   const hashtagDocuemts = await Promise.all(
+  //     hashtags.map((hashtag) => {
+  //       // Tìm hashtag trong database, nếu có thì lấy, không thì tạo mới
+  //       return databaseService.hashtags.findOneAndUpdate(
+  //         { name: hashtag },
+  //         {
+  //           $setOnInsert: new Hashtag({ name: hashtag })
+  //         },
+  //         {
+  //           upsert: true,
+  //           returnDocument: 'after'
+  //         }
+  //       )
+  //     })
+  //   )
+  //   // return hashtagDocuemts.map((hashtag) => (hashtag.value as WithId<Hashtag>)._id)
+  //   return hashtagDocuemts.map((hashtag) => (hashtag as any).value._id)
+  // }
+
+  async checkAndCreateHashtags(hashtags: string[]) {
+    const hashtagDocuments = await Promise.all(
+      hashtags.map(async (hashtag) => {
+        // Upsert hashtag
+        await databaseService.hashtags.updateOne(
+          { name: hashtag },
+          {
+            $setOnInsert: new Hashtag({ name: hashtag })
+          },
+          { upsert: true }
+        )
+
+        // Tìm lại hashtag để lấy _id
+        return await databaseService.hashtags.findOne({ name: hashtag })
+      })
+    )
+
+    return hashtagDocuments.map((hashtag) => hashtag!._id)
+  }
+
   async createTweet(user_id: string, body: TweetRequestBody) {
+    const hashtags = await this.checkAndCreateHashtags(body.hashtags)
     const result = await databaseService.tweets.insertOne(
       new Tweet({
         audience: body.audience,
         content: body.content,
-        hashtags: [], // Chỗ này chưa làm, tạm thời để rỗng
+        hashtags,
         mentions: body.mentions,
         medias: body.medias,
         parent_id: body.parent_id,
