792cc1da672eecefe5c347543853c48026de9936
DucNghia2004 <caoducnghia1605@gmail.com>
2026-01-09 10:11:55 +0700

    Tạo tweet

--- src/models/schemas/Tweet.schema.ts
+++ src/models/schemas/Tweet.schema.ts
@@ -8,12 +8,12 @@ interface TweetConstructor {
   type: TweetType
   audience: TweetAudience
   content: string
-  parent_id: null | ObjectId //  chỉ null khi tweet gốc
-  hashtags: ObjectId[]
-  mentions: ObjectId[]
+  parent_id: null | string //  chỉ null khi tweet gốc
+  hashtags: ObjectId[] //nếu hashtags có trong db rồi thì chúng ta sẽ lấy cái _id đó, còn nếu chưa có thì sẽ tạo mới và lấy _id đó
+  mentions: string[]
   medias: Media[]
-  guest_views: number
-  user_views: number
+  guest_views?: number
+  user_views?: number
   created_at?: Date
   updated_at?: Date
 }
@@ -53,12 +53,12 @@ export default class Tweet {
     this.type = type
     this.audience = audience
     this.content = content
-    this.parent_id = parent_id
+    this.parent_id = parent_id ? new ObjectId(parent_id) : null
     this.hashtags = hashtags
-    this.mentions = mentions
+    this.mentions = mentions.map((item) => new ObjectId(item)) // chuyển mảng string sang mảng ObjectId
     this.medias = medias
-    this.guest_views = guest_views
-    this.user_views = user_views
+    this.guest_views = guest_views || 0
+    this.user_views = user_views || 0
     this.created_at = created_at || date
     this.updated_at = updated_at || date
   }
